815e466420e3af6db8505bf59425197d02155870
Suin Kim <suin_ruth_kim20@naver.com>
2023-03-18 00:46:28 +0900

    second tray

--- src/main/java/com/hoteldream/domain/Member.java
+++ src/main/java/com/hoteldream/domain/Member.java
@@ -1,4 +1,15 @@
 package com.hoteldream.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Member {
+    @Id @GeneratedValue
+    private long id;
 }
